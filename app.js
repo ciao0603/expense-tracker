a217b111637bc0e7647008087134a8f26fe6d1db
Jocelyn Liu <jocelyn94032.0@gmail.com>
2023-07-25 10:36:25 +0800

    feat: add handlebars & bootstrap

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
 const express = require('express')
-const app = express()
+const exphbs = require('express-handlebars')
 
-if(process.env.NODE_ENV !== 'production') {
+if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
-
 require('./config/mongoose')
 
+const app = express()
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+
 app.get('/', (req, res) => {
-  res.send('success')
+  res.render('index')
 })
 
 const port = 3000
-app.listen(port, () =>{
+app.listen(port, () => {
   console.log(`Running on localhast:${port}`)
 })
