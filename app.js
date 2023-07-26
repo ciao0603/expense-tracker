135f5f0789a93fe881da1c9f07e383df1efa60b5
Jocelyn Liu <jocelyn94032.0@gmail.com>
2023-07-26 21:22:38 +0800

    feat: add edit function

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const dayjs = require('dayjs')
+const methodOverride = require('method-override')
 
 const Record = require('./models/record')
 
@@ -13,7 +14,9 @@ const app = express()
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }))
+
+app.use(methodOverride('_method'))
 
 app.get('/', (req, res) => {
   Record
@@ -32,15 +35,39 @@ app.get('/records/new', (req, res) => {
   res.render('new')
 })
 app.post('/records', (req, res) => {
-  const {name, date, category, amount} = req.body
+  const { name, date, category, amount } = req.body
   Record
     .create({ name, date, category, amount })
     .then(() => res.redirect('/'))
     .catch(err => console.log(err))
 })
 // 修改
-app.get('/records/edit', (req, res) => {
-  res.render('edit')
+app.get('/records/:id/edit', (req, res) => {
+  const id = req.params.id
+  Record
+    .findById(id)
+    .lean()
+    .then(record => {
+      record.date = dayjs(record.date).format('YYYY-MM-DD')
+      res.render('edit', { record })
+    })
+    .catch(err => console.log(err))
+})
+
+app.put('/records/:id', (req, res) => {
+  const id = req.params.id
+  const { name, date, category, amount } = req.body
+  Record
+    .findById(id)
+    .then(record => {
+      record.name = name
+      record.date = date
+      record.category = category
+      record.amount = amount
+      return record.save()
+    })
+    .then(() => res.redirect('/'))
+    .catch(err => console.log(err))
 })
 
 
