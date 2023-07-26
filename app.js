2fce57c4bee9209062b57220cccdeea959514af0
Jocelyn Liu <jocelyn94032.0@gmail.com>
2023-07-26 16:39:42 +0800

    feat: modify index page

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 
+const Record = require('./models/record')
+
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
@@ -11,7 +13,11 @@ app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
 app.get('/', (req, res) => {
-  res.render('index')
+  Record
+    .find()
+    .lean()
+    .then(records => res.render('index', { records }))
+    .catch(err => console.log(err))
 })
 app.get('/records/new', (req, res) => {
   res.render('new')
