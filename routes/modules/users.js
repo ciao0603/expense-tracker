dbb3a08124733b409178b1842ff5431c57e961fa
Jocelyn Liu <jocelyn94032.0@gmail.com>
2023-07-27 20:49:08 +0800

    feat: add registration feature

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const router = express.Router()
 
+const User = require('../../models/user')
+
 // login
 router.get('/login', (req, res) => {
   res.render('login')
@@ -8,6 +10,18 @@ router.get('/login', (req, res) => {
 // register
 router.get('/register', (req, res) => {
   res.render('register')
+})
+router.post('/register', async(req, res) => {
+  const { name, email, password, confirmPassword } = req.body
+  const user = await User.findOne({email})
+  if(user) {
+    console.log('user exist')
+    res.redirect('/users/login')
+  }
+  await User
+    .create({ name, email, password })
+    .then(() => res.redirect('/'))
+    .catch(err => console.log(err))
 })
 // logout
 router.get('/logout', (req, res) => {
