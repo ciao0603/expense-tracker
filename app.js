ea3f06645dcf2e160fe9a436d9b60d7b460e4a23
Jocelyn Liu <jocelyn94032.0@gmail.com>
2023-07-27 21:13:36 +0800

    feat: add express-session

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
+const session = require('express-session')
 
 const routes = require('./routes')
 
@@ -17,9 +18,15 @@ app.use(express.urlencoded({ extended: true }))
 
 app.use(methodOverride('_method'))
 
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true
+}))
+
 app.use(routes)
 
-const port = 3000
+const port = process.env.PORT
 app.listen(port, () => {
   console.log(`Running on localhast:${port}`)
 })
