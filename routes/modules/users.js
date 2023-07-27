876c89e0d9ca79b90e8a58d40c31be9db8322282
Jocelyn Liu <jocelyn94032.0@gmail.com>
2023-07-27 20:24:16 +0800

    feat: add register page

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -5,6 +5,10 @@ const router = express.Router()
 router.get('/login', (req, res) => {
   res.render('login')
 })
+// register
+router.get('/register', (req, res) => {
+  res.render('register')
+})
 // logout
 router.get('/logout', (req, res) => {
   res.send('logout')
