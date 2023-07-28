8ea89aa63f9a80e24a02bfe9eaaf658c09a9ecab
Jocelyn Liu <jocelyn94032.0@gmail.com>
2023-07-28 10:23:49 +0800

    feat: add connect-flash

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
 const session = require('express-session')
+const flash = require('connect-flash')
 
 const routes = require('./routes')
 const usePassport = require('./config/passport')
@@ -27,9 +28,13 @@ app.use(session({
 
 usePassport(app)
 
+app.use(flash())
+
 app.use((req, res, next) => {
   res.locals.isAuthenticated = req. isAuthenticated()
   res.locals.user = req.user
+  res.locals.waring_msg  = req.flash('warning_msg')
+  res.locals.success_msg = req.flash('success_msg')
   next()
 })
 
